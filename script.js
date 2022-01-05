fb98e94d9f1d049f5381462455ff50450595df67
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2022-01-05 21:59:31 +0530

    changed css with js and code refactor.

--- script.js
+++ script.js
@@ -54,8 +54,7 @@ document.querySelector('.check').addEventListener('click',function(){
 })
 
 document.querySelector('.again').addEventListener('click',function(){
-    score=20;
-    document.querySelector('.score').textContent=score;
+    document.querySelector('.score').textContent=20;
     number=Math.trunc(Math.random()*20)+1;
     document.querySelector('body').style.backgroundColor='#222';
     document.querySelector('.number').style.width='15rem';
